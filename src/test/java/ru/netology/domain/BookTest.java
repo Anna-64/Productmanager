1097b5b2eabcc2dda66b0609e99ecea6cc4d85e9
Anna-64 <shurygina6410@gmail.com>
2022-02-13 00:03:38 +0400

    added

---
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    public void shouldHaveMethodsFromSuper(){
+        Book book = new Book();
+//        book.
+    }
+
+}
