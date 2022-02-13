8dd8069c253a8f23351feb82b52dd3180a838040
Anna-64 <shurygina6410@gmail.com>
2022-02-13 13:34:44 +0400

    added

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -4,9 +4,47 @@ import ru.netology.domain.Product;
 import ru.netology.repository.ProductRepository;
 
 public class ProductManager {
+    int matches;
+
     private ProductRepository repository;
 
     public ProductManager(ProductRepository repository) { this.repository = repository; }
 
-    public void add(Product item) {repository.save(item);}
+    public void add(Product product) {repository.save(product);}
+
+    public void removeById(int id){ repository.removeById(id);}
+
+//    public Product[] getAll(){
+//        Product[] items = repository.findAll();
+//        Product[] result = new Product[items.length];
+//        for (int i = 0; i < result.length; i++){
+//            int index = items.length - i -1;
+//            result[i] = items[index];
+//        }
+//    return result;
+//    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0]; // тут будем хранить подошедшие запросу продукты
+        for (Product product: repository.findAll()) {
+            if (matches(product, text)) {
+                // "добавляем в конец" массива result продукт product
+                int index = items.length - i -1;
+                result[i] = items[index];
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+        // или в одну строку:
+        // return product.getName().contains(search);
+    }
 }
+
+
---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Product;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+
+    @Test
+    public void test(){
+        Product first = new Product(1)
+    }
+
+}
